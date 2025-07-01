fca1418da6e3091c5a14970a4bfade0c43f3f5c4
Markussan456 <209953316+Markussan456@users.noreply.github.com>
2025-07-01 17:02:02 +0300

    Add visually working display and buttons for calculator

--- math.js
+++ math.js
@@ -34,6 +34,30 @@ let numbers = document.querySelector(".numbers");
 for(let i = 1 ;i <= 9;i++){
 let digits = document.createElement("button");
 digits.classList.add("digit");
+digits.classList.add("calcbtn");
 digits.textContent = i;
 numbers.appendChild(digits);
+
+
 }
+let display = document.querySelector(".text");
+let buttons = document.querySelectorAll(".calcbtn");
+buttons.forEach(button =>{
+    button.addEventListener('click',()=>{
+        let btntext = button.textContent;
+        display.textContent += btntext;
+    })
+})
+let deletebtn = document.querySelector(".delete");
+deletebtn.addEventListener("click",()=>{
+    let arr = display.textContent.split("");
+    log(arr);
+    arr.pop();
+    log(arr);
+    let newstr = arr.join("");
+    log(newstr);
+    display.textContent = newstr; 
+    
+})
+let clearbtn = document.querySelector(".clearbtn");
+clearbtn.addEventListener("click",()=>{display.textContent = ""});
