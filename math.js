c5daa07586bc29441e72acdf7b1b3c33fd99bc86
Markussan456 <209953316+Markussan456@users.noreply.github.com>
2025-07-01 15:13:41 +0300

    Add styling for calculator frame and add switch case for calculator function

--- math.js
+++ math.js
@@ -13,4 +13,20 @@ function divide(num1,num2){
 function log(whatever){
     return console.log(whatever);
 }
+function operate(num1,num2,operator){
+    switch(operator)
+    {
+        case'+':
+        return add(num1,num2);
+        case'-':
+        return subtract(num1,num2);
+        case'*':
+        return multiply(num1,num2);
+        case'/':
+        return divide(num1,num2);
+        default:
+            return "Invalid operator";
+
+    }
+}
 
